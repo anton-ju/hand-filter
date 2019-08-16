c392d06d65868b7b4cea58edf30a0ede000e50b3
Anton Ju <anton.ju@outlook.com>
2019-08-16 21:32:06 +0300

    added *.txt extension, help usage

--- sort-tournament-position.py
+++ sort-tournament-position.py
@@ -13,8 +13,8 @@ output_dir = 'output'
 storage = HandStorage(cwd.joinpath(input_dir))
 
 
-# this script filters hh of sats with 4max tables
-def main():
+# this script filters hh of sats with 4 playrs tables, suits for 4max and for 3max
+def sort_by_tournament_position():
     result = []
     pos_codes = set()
     for txt in storage.read_hand():
@@ -32,11 +32,12 @@ def main():
             pos_str = str(player_pos['BU']) + str(player_pos['SB']) + str(player_pos['BB'])
         else:
             continue
+        # for ex. "1234" if CO has 1 stack BU 2 stack SB -3 BB -4
         pos_codes.add(pos_str)
         result_row = {'pos_code': pos_str, 'txt': txt, 'fn': hh.hid}
         result.append(result_row)
     output_dir_path = cwd.joinpath(output_dir)
-    # create all output directories
+    # create all possible output directories from pos_codes
     if not output_dir_path.exists():
         output_dir_path.mkdir()
     for pos_code in pos_codes:
@@ -46,23 +47,44 @@ def main():
 
     for row in result:
         res_dir_path = output_dir_path.joinpath(row['pos_code'])
-        res_file_path = res_dir_path.joinpath(row['fn'])
+        res_file_path = res_dir_path.joinpath(row['fn']).with_suffix('.txt')
         res_file_path.write_text(row['txt'], encoding='utf-8')
 
 
+def main(options):
+    if options.save:
+        sort_by_tournament_position()
+
+
 if __name__ == '__main__':
-    op = OptionParser()
-    op.add_option("-l", "--log", action="store", default=None)
-    (opts, args) = op.parse_args()
-    logging.basicConfig(filename=opts.log,
+    usage = "usage: %prog [options] arg1 arg2"
+    op = OptionParser(usage=usage)
+    op.add_option("-l", "--log", action="store", default=None,
+                  dest="log",
+                  help="log file")
+    op.add_option("-s", "--save",
+                  action="store_true",
+                  default=True,
+                  dest="save",
+                  help="save results in different folders according to the relative stack size")
+    op.add_option("-f", "--filter",
+                  action="store",
+                  default="less",
+                  dest="filter",
+                  help="filter hands if one of the players has stack: less, greater "
+                  " [default: %default]")
+    (options, args) = op.parse_args()
+    logging.basicConfig(filename=options.log,
                         level=logging.DEBUG,
                         format='[%(asctime)s] %(levelname).1s %(message)s',
                         datefmt='%Y.%m.%d %H:%M:%S')
     logger = logging.getLogger(__name__)
 
-    if opts.log:
-        fh = logging.FileHandler(opts.log)
+    if options.log:
+        fh = logging.FileHandler(options.log)
         logger.addHandler(fh)
     ch = logging.StreamHandler()
     logger.addHandler(ch)
-    main()
+    if len(args) == 0:
+        op.print_help()
+    main(options)
